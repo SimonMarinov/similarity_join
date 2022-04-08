95c3c23df6f29f52842a74a3f4f41ebd4cc53eef
simon <marinov.simon>
2022-04-08 16:35:03 +0200

    Initial commit

---
+++ src/main/java/com/marinsim/similarity_join/SimilarityJoinApplication.java
@@ -0,0 +1,13 @@
+package com.marinsim.similarity_join;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimilarityJoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimilarityJoinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marinsim/similarity_join/SimilarityJoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marinsim.similarity_join;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarityJoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
